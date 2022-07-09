aa72cf5d8b96d7bbd36043b6f1e7627e5d029300
Luuk van den Maagdenberg <luuk.vdm@hotmail.com>
2022-07-09 20:12:13 +0200

    moved LoadPNG() to resources.go

--- src/media/resources.go
+++ src/media/resources.go
@@ -2,6 +2,7 @@ package media
 
 import (
 	_ "embed"
+	"fmt"
 	"github.com/diamondburned/gotk4/pkg/gdkpixbuf/v2"
 	"log"
 )
@@ -24,4 +25,22 @@ func init() {
 	JumperPixelBuffs = PixelBuffs{
 		gopher,
 	}
+}
+
+func LoadPNG(data []byte) (*gdkpixbuf.Pixbuf, error) {
+	l, err := gdkpixbuf.NewPixbufLoaderWithType("png")
+	if err != nil {
+		return nil, fmt.Errorf("NewLoaderWithType png: %w", err)
+	}
+	defer l.Close()
+
+	if err := l.Write(data); err != nil {
+		return nil, fmt.Errorf("PixbufLoader.Write: %w", err)
+	}
+
+	if err := l.Close(); err != nil {
+		return nil, fmt.Errorf("PixbufLoader.Close: %w", err)
+	}
+
+	return l.Pixbuf(), nil
 }
